f792cda124abe9809616f500501a041cf8920339
L.Grigoreva <l.grigoreva@MacBook-Pro-X5.local>
2021-08-24 20:10:15 +0300

    phoneNumberField limited to 10 symbols and added +7

--- StatefulScreenExample/RIBs/Authorization/AuthorizationViewController.swift
+++ StatefulScreenExample/RIBs/Authorization/AuthorizationViewController.swift
@@ -13,8 +13,11 @@ import UIKit
 
 final class AuthorizationViewController: UIViewController, AuthorizationViewControllable {
     
-    @IBOutlet private weak var phoneNumberField: AuthorizationField!
+    @IBOutlet private weak var phoneNumberField: UITextField!
     @IBOutlet private weak var sendCodeButton: UIButton!
+    
+    let plusLabel = UILabel()
+    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 38, height: 46))
     
     // MARK: View Events
     
@@ -27,13 +30,27 @@ final class AuthorizationViewController: UIViewController, AuthorizationViewCont
         view.backgroundColor = .systemBackground
         
         initialSetup()
-//        tapGesturesInitialSetup()
     }
 }
 
 extension AuthorizationViewController {
     private func initialSetup() {
-        phoneNumberField.design()
+        plusLabel.text = "+7"
+        plusLabel.font = UIFont.systemFont(ofSize: 17)
+        plusLabel.textColor = UIColor(hexString: "#ACAAB2")
+        plusLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        paddingView.addSubview(plusLabel)
+        plusLabel.leftAnchor.constraint(equalTo: paddingView.leftAnchor, constant: 16).isActive = true
+        plusLabel.rightAnchor.constraint(equalTo: paddingView.rightAnchor, constant: -6).isActive = true
+        plusLabel.centerYAnchor.constraint(equalTo: paddingView.centerYAnchor).isActive = true
+        
+        phoneNumberField.leftView = paddingView
+        phoneNumberField.leftViewMode = .always
+        
+        phoneNumberField.tintColor = UIColor(hexString: "#34BC48")
+
+        phoneNumberField.delegate = self.phoneNumberField
     }
 }
 
@@ -53,7 +70,7 @@ extension AuthorizationViewController: BindableView {
 extension AuthorizationViewController {
     private struct ViewOutput: AuthorizationViewOutput {
         
-        @PublishControlEvent var phoneNumberUpdateTap: ControlEvent<String?>
+        @PublishControlEvent var phoneNumberUpdateTap: ControlEvent<String>
         
         @PublishControlEvent var sendCodeButtonTap: ControlEvent<Void>
         
